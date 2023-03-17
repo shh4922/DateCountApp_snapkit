fa1bb9df7a1ff0f9e1ec69493af3efe35a99e715
sinhyeonho <gusgh4922@gmail.com>
2023-03-17 13:34:33 +0900

    어케 해야하는걸까... 수정중인데 모르는부분이 많아서 선 공부 후 수정을 해야할것같다...

--- DateCountApp_snapkit/LoginVC.swift
+++ DateCountApp_snapkit/LoginVC.swift
@@ -4,31 +4,49 @@ import SnapKit
 
 class LoginVC: UIViewController {
     
+    private let containerview = UIView()
     private var idField = UITextField()
     private var passField = UITextField()
     private var loginLabel = UILabel()
     private var subLabel = UILabel()
-    private let scrollView = UIScrollView()
+    private var scrollView = UIScrollView()
     private let loginButton = UIButton()
     private let signUpButton = UIButton()
+    
+    private var keyHeight = UIView()
+    private var nskey = NSLayoutConstraint()
     
     private func setLayout(){
         navigationController?.navigationBar.prefersLargeTitles = true
         self.view.backgroundColor = .white
         view.addSubview(scrollView)
     
+        scrollView.addSubview(containerview)
         scrollView.addSubview(loginLabel)
         scrollView.addSubview(subLabel)
         scrollView.addSubview(idField)
         scrollView.addSubview(passField)
         scrollView.addSubview(loginButton)
         scrollView.addSubview(signUpButton)
+        scrollView.addSubview(keyHeight)
         
         scrollView.snp.makeConstraints { make in
             make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).offset(0)
             make.leading.equalTo(self.view.safeAreaLayoutGuide.snp.leading).offset(0)
             make.trailing.equalTo(self.view.safeAreaLayoutGuide.snp.trailing).offset(0)
-            make.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.bottom).offset(0)
+            make.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.bottom).offset(-1)
+        }
+        keyHeight.snp.makeConstraints { make in
+            make.top.equalTo(scrollView.snp.bottom).offset(0)
+            make.leading.equalTo(scrollView.snp.leading).offset(0)
+            make.trailing.equalTo(scrollView.snp.trailing).offset(0)
+            make.bottom.equalTo(scrollView.snp.bottom).offset(0)
+        }
+        containerview.snp.makeConstraints { make in
+            make.top.equalTo(scrollView.snp.top).offset(0)
+            make.leading.equalTo(scrollView.snp.leading).offset(0)
+            make.trailing.equalTo(scrollView.snp.trailing).offset(0)
+            make.bottom.equalTo(scrollView.snp.bottom).offset(0)
         }
         
         loginLabel.textColor = .black
@@ -37,8 +55,8 @@ class LoginVC: UIViewController {
         loginLabel.font = .boldSystemFont(ofSize: 40)
         loginLabel.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
-            make.top.equalTo(scrollView.snp.top).offset(20)
-            make.leading.equalTo(scrollView.snp.leading).offset(30)
+            make.top.equalTo(containerview.snp.top).offset(20)
+            make.leading.equalTo(containerview.snp.leading).offset(30)
         }
         
         subLabel.text = "하루명언으로 항상 동기부여받으며 공부해요."
@@ -48,7 +66,7 @@ class LoginVC: UIViewController {
         subLabel.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.top.equalTo(loginLabel.snp.bottom).offset(5)
-            make.leading.equalTo(scrollView.snp.leading).offset(30)
+            make.leading.equalTo(containerview.snp.leading).offset(30)
         }
         
         idField.placeholder = "id"
@@ -58,7 +76,7 @@ class LoginVC: UIViewController {
         idField.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.top.equalTo(subLabel.snp.bottom).offset(30)
-            make.leading.equalTo(scrollView.snp.leading).offset(40)
+            make.leading.equalTo(containerview.snp.leading).offset(40)
         }
         
         passField.placeholder = "password"
@@ -68,7 +86,7 @@ class LoginVC: UIViewController {
         passField.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.top.equalTo(idField.snp.bottom).offset(10)
-            make.leading.equalTo(scrollView.snp.leading).offset(40)
+            make.leading.equalTo(containerview.snp.leading).offset(40)
         }
         
         loginButton.backgroundColor = .systemBlue
@@ -78,7 +96,7 @@ class LoginVC: UIViewController {
         loginButton.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.top.equalTo(passField.snp.bottom).offset(40)
-            make.leading.equalTo(scrollView.snp.leading).offset(100)
+            make.leading.equalTo(containerview.snp.leading).offset(100)
         }
         
         signUpButton.setTitle("create your account", for: .normal)
@@ -86,7 +104,7 @@ class LoginVC: UIViewController {
         signUpButton.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.top.equalTo(loginButton.snp.bottom).offset(5)
-            make.leading.equalTo(scrollView.snp.leading).offset(20)
+            make.leading.equalTo(containerview.snp.leading).offset(20)
         }
         
     }
@@ -100,41 +118,47 @@ class LoginVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setKeyboardObserver()
     }
     
+    
+    private func addKeyboardObserver(){
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object:nil)
+    }
     
 }
 
 
+
+
 extension UIViewController {
-    
+
     func setKeyboardObserver() {
         NotificationCenter.default.addObserver(self, selector: #selector(UIViewController.keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(UIViewController.keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object:nil)
     }
     
-    @objc func keyboardWillShow(notification: NSNotification) {
-        if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
-                  let keyboardRectangle = keyboardFrame.cgRectValue
-                  let keyboardHeight = keyboardRectangle.height
-              UIView.animate(withDuration: 1) {
-                  self.view.window?.frame.origin.y -= keyboardHeight
-              }
+
+    
+    @objc func keyboardWillShow(_ sender: Notification) {
+        
+            let userInfo:NSDictionary = sender.userInfo! as NSDictionary
+            let keyboardFrame:NSValue = userInfo.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue
+            let keyboardRectangle = keyboardFrame.cgRectValue
+            let keyboardHeight = keyboardRectangle.height
+//            keyHeight = keyboardHeight
+        print("keyboard run")
+            self.view.frame.size.height -= keyboardHeight
+    }
+    @objc func keyboardWillHide(_ sender: Notification) {
+        
+        self.view.frame.size.height += Keybo
+        if let height = keyHeight {
+            self.view.frame.size.height += height
         }
       }
-    
-    @objc func keyboardWillHide(notification: NSNotification) {
-        if self.view.window?.frame.origin.y != 0 {
-            if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
-                    let keyboardRectangle = keyboardFrame.cgRectValue
-                    let keyboardHeight = keyboardRectangle.height
-                UIView.animate(withDuration: 1) {
-                    self.view.window?.frame.origin.y += keyboardHeight
-                }
-            }
-        }
-    }
-    
+
 }
 
 
