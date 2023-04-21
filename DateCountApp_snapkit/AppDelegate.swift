aabd4c576cba4d6e218803c868e52f55dc6e12ad
hyeonho <gusgh4922@gmail.com>
2023-04-21 23:08:55 +0900

    11:08

--- DateCountApp_snapkit/AppDelegate.swift
+++ DateCountApp_snapkit/AppDelegate.swift
@@ -5,8 +5,8 @@ import Firebase
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
     
-    
-    
+    //stop
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
         
